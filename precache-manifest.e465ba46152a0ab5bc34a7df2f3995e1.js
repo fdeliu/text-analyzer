b76ee188d1bee7e254f2bed9051dd20bf9156dc3
unknown <Flamur Deliu>
2019-11-07 16:40:06 +0100

    Updates

---
+++ precache-manifest.e465ba46152a0ab5bc34a7df2f3995e1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0fb75fe148029492ac4deb568c63501",
+    "url": "/text-analyzer/index.html"
+  },
+  {
+    "revision": "714cd3a352384b20926a",
+    "url": "/text-analyzer/static/css/main.d7c63673.chunk.css"
+  },
+  {
+    "revision": "30ffcf8b5f57b524f9e3",
+    "url": "/text-analyzer/static/js/2.4df0cb8d.chunk.js"
+  },
+  {
+    "revision": "714cd3a352384b20926a",
+    "url": "/text-analyzer/static/js/main.c032e207.chunk.js"
+  },
+  {
+    "revision": "98b201bb80a6f89b034c",
+    "url": "/text-analyzer/static/js/runtime-main.16b34948.js"
+  },
+  {
+    "revision": "bf4c97ed096fec8cdbdbdeeea9a82e72",
+    "url": "/text-analyzer/static/media/loading.bf4c97ed.gif"
+  }
+]);
